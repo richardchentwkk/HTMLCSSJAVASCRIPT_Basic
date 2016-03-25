b824bbcb242acc4748dc13c4ffd341ac06213dc2
richardchentwkk <richardchentwkk@gmail.com>
2016-03-25 23:31:53 +0800

    HW5 Multi-tab finished change Tab part.

--- Homework/Multi_Tab/js/tab.js
+++ Homework/Multi_Tab/js/tab.js
@@ -1,1 +1,20 @@
+function changeContent(tabNum) {
+    var contents =
+        new Array(getId("content1"), getId("content2"), getId("content3"));
+    var tabs =
+        new Array(getId("tab1"), getId("tab2"), getId("tab3"));
 
+    for (i = 0; i < contents.length; i++) {
+        if (i === tabNum) {
+            tabs[i].style.backgroundColor = "white";
+            contents[i].style.display = "block";
+        } else {
+            tabs[i].style.backgroundColor = "gray";
+            contents[i].style.display = "none";
+        }
+    }
+}
+
+function getId(id) {
+    return document.getElementById(id);
+}
